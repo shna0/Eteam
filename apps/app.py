c43d9e1573377242e79ec307b659d2df8e520dd2
shna0 <shuuuma.0605@gmail.com>
2024-11-12 14:35:01 +0900

    test

---
+++ apps/app.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from apps.config import config
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+csrf = CSRFProtect()
+
+login_manager = LoginManager()
+
+login_manager.login_view = "auth.signup"
+
+login_manager.login_message = ""
+
+def create_app(config_key):
+    app = Flask(__name__)
+
+    app.config.from_object(config[config_key])
+
+
+    # app.config.from_mapping(
+    #     SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
+    #     SQLALCHEMY_DATABASE_URI="sqlite:///" + 
+    #     str(Path(Path(__file__).parent.parent , "local.sqlite")),
+    #     SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    #     SQLALCHEMY_ECHO=True,
+    #     WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f",
+    # )
+
+    csrf.init_app(app)
+
+    db.init_app(app)
+
+    Migrate(app,db)
+
+    login_manager.init_app(app)
+
+    from apps.crud import views as crud_views
+
+    app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    from apps.auth import views as auth_views
+
+    app.register_blueprint(auth_views.auth, url_prefix="/auth")
+
+    from apps.detector import views as dt_views
+
+    app.register_blueprint(dt_views.dt)
+
+    return app
